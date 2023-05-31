dc45c930faa8c053149fd2551d076415de59a775
preetgami <109097825+preetgami@users.noreply.github.com>
2023-05-31 12:58:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -33,5 +33,5 @@ app.use((error, req, res, next) => {
 })
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@atlascluster.ekohyyj.mongodb.net/?retryWrites=true&w=majority`).then(
-    () => app.listen(5000)
+    () => app.listen(process.env.PORT || 5000)
 ).catch(err => console.log("i found an error"))
